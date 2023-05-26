c2a761e139b08d470c4cceea01456fe7450d11eb
bondanjody <bondanjodys@gmail.com>
2023-05-26 11:55:54 +0700

    Merapikan file login.php dan mengubah link register

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -17,8 +17,7 @@
 
                                     <form class="user" method="post" action="<?= base_url('autentifikasi/') ?>">
                                         <div class="form-group">
-                                            <input type="email" class="form-control formcontrol-user" id="exampleInputEmail" aria-describedby="emailHelp"
-                                            placeholder="Enter Email Address...">
+                                            <input type="email" class="form-control formcontrol-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                                             <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                         </div>
                                         <div class="form-group">
@@ -27,25 +26,17 @@
                                             <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                         </div>
                                         <div class="form-group">
-                                            <div class="custom-control custom-checkbox
-                                            small">
-                                                <input type="checkbox" class="customcontrol-input" id="customCheck">
-                                                <label class="custom-control-label"
-                                                for="customCheck">Remember Me</label>
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember Me</label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btnuser btn-block">
-                                        Login
-                                        </a>
+                                        <a href="index.html" class="btn btn-primary btnuser btn-block"> Login </a>
                                         <hr>
                                         <a href="index.html" class="btn btn-google btnuser btn-block">
-                                        <i class="fab fa-google fa-fw"></i> Login with
-                                        Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook
-                                        btn-user btn-block">
-                                        <i class="fab fa-facebook-f fa-fw"></i> Login
-                                        with Facebook
+                                        <i class="fab fa-google fa-fw"></i> Login with Google</a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                         </a>
                                     </form>
                                     <hr>
@@ -53,8 +44,7 @@
                                         <a class="small" href="<?= base_url('autentifikasi/lupaPassword'); ?>forgotpassword.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="<?= base_url('autentifikasi/registrasi'); ?>register.html">Create an
-                                        Account!</a>
+                                        <a class="small" href="<?= base_url('autentifikasi/'); ?>register.php">Create an Account!</a>
                                     </div>
                             </div>
                         </div>
